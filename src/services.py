0c3b43b2f90bbdede2d9fb858624ad05fd016429
35347 <353472010401150@mail.ru>
2025-03-03 22:26:01 +0300

    new

--- src/services.py
+++ src/services.py
@@ -13,6 +13,8 @@ def read_json_file(filename):
     return data
 
 
+from datetime import datetime  # Добавляем импорт
+
 def analyze_cashback(data, year, month):
     """Сервис позволяет проанализировать, какие категории были наиболее выгодными для выбора
     в качестве категорий повышенного кешбэка."""
@@ -25,29 +27,16 @@ def analyze_cashback(data, year, month):
     for category in categories:
         total_cashback = 0
         for transaction in transactions:
+            # Преобразуем строку даты в объект datetime
+            transaction_date = datetime.strptime(transaction["date"], "%Y-%m-%d")
             if (
                 transaction["category"] == category
-                and transaction["date"].year == year
-                and transaction["date"].month == month
+                and transaction_date.year == year
+                and transaction_date.month == month
             ):
                 total_cashback += transaction["cashback"]
         results[category] = total_cashback
 
     logging.info("Анализ кэшбэка за %s год, %s месяц успешно завершён", year, month)
     return results
-
-
-#Пример использования функции
-data = {
-    "transactions": [
-        {"category": "Категория 1", "cashback": 500, "date": datetime.datetime(2023, 1, 1)},
-        {"category": "Категория 2", "cashback": 1000, "date": datetime.datetime(2023, 2, 1)},
-    ],
-    "categories": ["Категория 1", "Категория 2"],
-}
-
-result = analyze_cashback(data, 2023, 2)
-
-# Форматированный вывод результата
-print(json.dumps(result, ensure_ascii=False, indent=4))
 
